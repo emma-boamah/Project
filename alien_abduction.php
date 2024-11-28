926399cacfed74da6f020735a5234d7f24ad485d
emma-boamah <emmanuelboamah046@gmail.com>
2024-11-27 17:42:30 -0800

    Validated user input #1

--- alien_abduction.php
+++ alien_abduction.php
@@ -13,6 +13,8 @@ if(isset($_POST["submit"])){
         echo "Connection failed". $e->getMessage();
     }
     // GET USER INPUTS
+    // VALIDATE INPUTS
+    $errors = [];
     $full_Name = htmlspecialchars($_POST["firstname"] ." ". $_POST["lastname"]);
     $email = htmlspecialchars($_POST["email"]);
     $when_it_happened = htmlspecialchars($_POST["date"]);
@@ -23,20 +25,40 @@ if(isset($_POST["submit"])){
     $cloth_found = htmlspecialchars($_POST["lost_cloth"]);
     $other = htmlspecialchars($_POST["other"]);
 
-    $stmt = $Connection->prepare("INSERT INTO alien_abduction(Full_Name, Email, when_it_happened, how_long, how_many, alien_description, what_they_did, cloth_found, other) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bindParam(1, $full_Name, PDO::PARAM_STR);
-    $stmt->bindParam(2, $email, PDO::PARAM_STR);
-    $stmt->bindParam(3, $when_it_happened, PDO::PARAM_STR);
-    $stmt->bindParam(4, $how_long, PDO::PARAM_STR);
-    $stmt->bindParam(5, $how_many, PDO::PARAM_STR);
-    $stmt->bindParam(6, $alien_description, PDO::PARAM_STR);
-    $stmt->bindParam(7, $what_they_did, PDO::PARAM_STR);
-    $stmt->bindParam(8, $cloth_found, PDO::PARAM_STR);
-    $stmt->bindParam(9, $other, PDO::PARAM_STR);
-
-    if($stmt->execute()){
-        echo "Inserted into DataBase";
-    } else{
-        echo "Failed to insert into DataBase";
+    if(empty($full_Name)){
+        $errors[] = "Please enter your first name or last name";
     }
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors[] = "Valid email is required!";
+    }
+
+    if(empty($when_it_happened)){
+        $errors[] = "Date of incident is required!";
+    }
+
+    if(!empty($errors)){
+        echo 'Errors:';
+        foreach($errors as $error){
+            echo '<br />'. $error .'';
+        }
+    }else{
+        $stmt = $Connection->prepare("INSERT INTO alien_abduction(Full_Name, Email, when_it_happened, how_long, how_many, alien_description, what_they_did, cloth_found, other) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bindParam(1, $full_Name, PDO::PARAM_STR);
+        $stmt->bindParam(2, $email, PDO::PARAM_STR);
+        $stmt->bindParam(3, $when_it_happened, PDO::PARAM_STR);
+        $stmt->bindParam(4, $how_long, PDO::PARAM_STR);
+        $stmt->bindParam(5, $how_many, PDO::PARAM_STR);
+        $stmt->bindParam(6, $alien_description, PDO::PARAM_STR);
+        $stmt->bindParam(7, $what_they_did, PDO::PARAM_STR);
+        $stmt->bindParam(8, $cloth_found, PDO::PARAM_STR);
+        $stmt->bindParam(9, $other, PDO::PARAM_STR);
+
+        if($stmt->execute()){
+            echo "Inserted into DataBase";
+        } else{
+            echo "Failed to insert into DataBase";
+        }
+    }
+
+    
 }
